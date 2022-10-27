5277897262ec4559064ef6c56b414e96eb0f5b50
Louis Bester <LouisB@fmi.co.za>
2022-10-27 12:59:03 +0200

    Added previously ignored models to git

---
+++ Headline.Common.Models/Headline.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Headline.Common.Models
+{
+    public class HeadlineModel
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("banner")]
+        public string Banner { get; set; } = "";
+
+        [JsonPropertyName("backgroundColour")]
+        public string BackgroundColour { get; set; } = "#000000";
+
+        [JsonPropertyName("foregroundColour")]
+        public string ForegroundColour { get; set; } = "#ffffff";
+
+        [JsonPropertyName("imageUrl")]
+        public string ImageUrl { get; set; } = "";
+
+        [JsonPropertyName("active")]
+        public bool Active { get; set; } = true;
+    }
+}
